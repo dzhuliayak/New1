8dcc93183cc016c016a9db75ef6f96217017f8ad
test1 <dzhuliayakuheva@gmail.com>
2024-11-14 01:19:08 +0300

    добавлена функция установки времени

--- Модуль 9.1 проект напоминание.py
+++ Модуль 9.1 проект напоминание.py
@@ -6,6 +6,23 @@ import datetime
 import time
 import pygame
 
+
+def set():
+    rem=sd.askstring("время напоминания","введите время напоминания в формате ЧЧ:ММ")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])#берем первую часть
+            minute=int(rem.split(":")[1])
+            now=datetime.datetime.now()
+            print(now)
+            dt=now.replace(hour=hour, minute=minute)
+            print(dt)
+            t=dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror("ошибка!", f"произошла ошибка {e}")
+
+
 window=Tk()
 window.title("напоминание")
 label=Label(text="установите напоминание")
